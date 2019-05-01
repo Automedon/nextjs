fa9fe79babcaaa26411e83e3d590b26fbaae161d
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-05-01 21:47:23 +0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,73 @@
-import Link from "next/link";
-import Header from "../components/header";
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
 
-function Index() {
-  return (
-    <main>
-      <Header />
-      <section>
-        <Link href="/about">
-          <a>Go to About Me</a>
-        </Link>
-      </section>
-    </main>
-  );
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ]
 }
 
-export default Index;
+function PostLink({ post }) {
+  return (
+    <li>
+      <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+        <a>{post.title}</a>
+      </Link>
+      <style jsx>{`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: 'Arial';
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </li>
+  )
+}
+
+export default function Index() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
+  )
+}
